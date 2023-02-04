6b8465c454ad18e6c50a57eb9de6549dbef8f802
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-02-04 22:55:43 +0000

    Added current weather ap instead of forecast api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ let cities = [];
 let searchButton = $("#search-button");
 let listOfCities = $(".input-group-append");
 let inputEl = $("#search-input");
-let currentWeatherEl = $("#today").addClass('today');
+let currentWeatherEl = $("#today");
+let forecastEl = $("#forecast");
+
 let cityName;
 
 searchButton.on("click", function(event) {
@@ -19,6 +21,10 @@ searchButton.on("click", function(event) {
     cityName = city;
     inputEl.val("");
     currentWeatherEl.empty();
+    forecastEl.empty();
+  }
+  else {
+    alert("Please, enter city");
   }
 
   $.ajax({
@@ -27,27 +33,42 @@ searchButton.on("click", function(event) {
   }).then(function(response) {
     let lat = response[0].lat;
     let lon = response[0].lon;
-    buildForecastQueryURL(lat, lon);
+    buildWeathertQueryURL(lat, lon);
 
     $.ajax({
-      url: buildForecastQueryURL(lat, lon),
+      url: buildWeathertQueryURL(lat, lon),
       method: "GET",
     }).then(function(response) {
       console.log(response);
+
+      currentWeatherEl.addClass('today');
       
-      let cityNameEl = $("<h3>").addClass('city-name').text(`${cityName}, `);;
-      let dateEl = $("<p>").text(moment().format("LL"));
-      let iconEl = $("<img>").attr('src', `http://openweathermap.org/img/w/${response.list[0].weather[0].icon}.png`);
+      let cityNameEl = $("<h3>").addClass('city-name').text(`${cityName}, `);
+      let currentDate = moment();
+
+      let dateEl = $("<p>").text(currentDate.format("LL"));
+      let iconEl = $("<img>").attr('src', `http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
       
       currentWeatherEl.append(cityNameEl);
       currentWeatherEl.append(dateEl);
       currentWeatherEl.append(iconEl);
 
-      currentWeatherEl.append($("<div>").text("Tempreture: " + Math.round(response.list[0].main.temp) + "ºC"));
-      currentWeatherEl.append($("<div>").text("Wind: " + (response.list[0].wind.speed).toFixed(1) + "km/h"));
-      currentWeatherEl.append($("<div>").text("Humidity: " + response.list[0].main.humidity + "%"));
+      currentWeatherEl.append($("<div>").text("Tempreture: " + Math.round(response.main.temp) + "ºC"));
+      currentWeatherEl.append($("<div>").text("Wind: " + (response.wind.speed).toFixed(1) + " m/s"));
+      currentWeatherEl.append($("<div>").text("Humidity: " + response.main.humidity + "%"));
 
+      let forecastTitleEl = $("<h3>").text("5-Day Forecast");
+      forecastEl.append(forecastTitleEl).addClass('forecast')
 
+      let citiesArray = response.list;
+      console.log("citiesArray", citiesArray);
+
+      for (let i = 1; i < 6; i++) {
+        let day = citiesArray.filter(item => splitOnDays(item, i))
+        console.log(day);
+        console.log('*************************');
+        //day.forEach(element => )
+      }
     });
   });
 });
@@ -64,12 +85,21 @@ function buildLocationQueryURL(city) {
   locationQueryParams.q = city;
   return locationQueryURL + $.param(locationQueryParams);
 }
-function buildForecastQueryURL(lat, lon) {
-  let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?";
-  let forecastQueryParams = { appid: "61ba8177c893a48d024315792d0535ca" };
-  forecastQueryParams.lat = lat;
-  forecastQueryParams.lon = lon;
-  forecastQueryParams.units = "metric";
-  console.log(forecastQueryURL + $.param(forecastQueryParams));
-  return forecastQueryURL + $.param(forecastQueryParams);
+
+function buildWeathertQueryURL(lat, lon) {
+  let weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?";
+  let weatherQueryParams = { appid: "61ba8177c893a48d024315792d0535ca" };
+  weatherQueryParams.lat = lat;
+  weatherQueryParams.lon = lon;
+  weatherQueryParams.units = "metric";
+  return weatherQueryURL + $.param(weatherQueryParams);
 }
+
+// function splitOnDays(item, i) {
+//   if 
+//   return (moment.unix(item.dt).format("DD/MM/YYYY") === (moment(new Date()).add(`${i}`, 'days')).format("DD/MM/YYYY"))
+// }
+
+
+
+
