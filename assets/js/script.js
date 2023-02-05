87e83633426cd49617f83ffa95e34c5047d9a2e0
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-02-05 10:53:08 +0000

    Added forecast for the first day

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,23 +34,29 @@ searchButton.on("click", function (event) {
     let lat = response[0].lat;
     let lon = response[0].lon;
     buildWeathertQueryURL(lat, lon);
+    console.log(buildWeathertQueryURL(lat, lon));
+    buildForecastQueryURL(lat, lon);
+    console.log(buildForecastQueryURL(lat, lon));
 
-    // Current weather query to get data to display at the top of the page
-    $.ajax({
-      url: buildWeathertQueryURL(lat, lon),
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
+    $.when(
+      $.ajax(buildWeathertQueryURL(lat, lon)),
+      $.ajax(buildForecastQueryURL(lat, lon))
+    ).done(function (response1, response2) {
+      console.log(response1[0]);
+      console.log(response2[0].list);
 
+      dayResponse = response1[0];
+      forecastResponse = response2[0].list;
+
+      // Display current weather
       currentWeatherEl.addClass("today");
-
       let cityNameEl = $("<h3>").addClass("city-name").text(`${cityName}, `);
       let currentDate = moment();
 
       let dateEl = $("<p>").text(currentDate.format("LL"));
       let iconEl = $("<img>").attr(
         "src",
-        `http://openweathermap.org/img/w/${response.weather[0].icon}.png`
+        `http://openweathermap.org/img/w/${dayResponse.weather[0].icon}.png`
       );
 
       currentWeatherEl.append(cityNameEl);
@@ -58,33 +64,108 @@ searchButton.on("click", function (event) {
       currentWeatherEl.append(iconEl);
 
       currentWeatherEl.append(
-        $("<div>").text("Tempreture: " + Math.round(response.main.temp) + "ºC")
+        $("<div>").text(
+          "Tempreture: " + Math.round(dayResponse.main.temp) + "ºC"
+        )
       );
       currentWeatherEl.append(
-        $("<div>").text("Wind: " + response.wind.speed.toFixed(1) + " m/s")
+        $("<div>").text("Wind: " + dayResponse.wind.speed.toFixed(1) + " m/s")
       );
       currentWeatherEl.append(
-        $("<div>").text("Humidity: " + response.main.humidity + "%")
+        $("<div>").text("Humidity: " + dayResponse.main.humidity + "%")
       );
-    });
 
-    // 5-day forecast to display
-    $.ajax({
-      url: buildForecastQueryURL(lat, lon),
-      method: "GET",
-    }).then(function (response) {
-      response = response.list;
-      console.log(response);
-      
-      let day1 = response.slice(0, 8);
-      console.log(day1);
-      let temp1 = [];
-      day1.forEach(function(item) {
-        temp1.push(item.main.temp)
-      })
-      console.log(temp1)
-      console.log(Math.round(Math.max(...temp1)));
-      console.log(Math.round(Math.min(...temp1)));
+      // Display forecast
+      const correctDay = (element) => moment(element.dt_txt).format("YYYY-MM-DD") === moment().add(1, 'days').format("YYYY-MM-DD");
+      let index = forecastResponse.findIndex(correctDay);
+      // To display data at 12:00 
+      index += 4;
+
+      let dateCardEl = $("<h5>").text(
+        moment("2023-02-06 00:00:00").format("DD/MM/YYYY")
+      );
+
+      let iconCardEl = $("<div>").append($("<img>").attr(
+        "src",
+        `http://openweathermap.org/img/w/${forecastResponse[index].weather[0].icon}.png`
+      ));
+
+      let tempCardEl = $("<p>").text("Temperature: " + 
+        Math.round(forecastResponse[index].main.temp)  + "ºC"
+      );
+
+      let windCardEl = $("<p>").text("Wind: " + forecastResponse[index].wind.speed.toFixed(1) + " m/s");
+
+      let humidityCardEl = $("<p>").text("Humidity: " + forecastResponse[index].main.humidity + "%");
+
+
+      $("#forecast").append(dateCardEl);
+      $("#forecast").append(iconCardEl);
+      $("#forecast").append(tempCardEl);
+      $("#forecast").append(windCardEl);
+      $("#forecast").append(humidityCardEl);
+      $("#forecast").addClass("card-container");
+
+
+      // console.log(temp1)
+      // console.log(Math.round(Math.max(...temp1)));
+      // console.log(Math.round(Math.min(...temp1)));
+
+      // let minTemp = Math.round(Math.min(...temp1));
+      // let maxTemp = Math.round(Math.max(...temp1));
+
+      // // Current weather query to get data to display at the top of the page
+      // $.ajax({
+      //   url: buildWeathertQueryURL(lat, lon),
+      //   method: "GET",
+      // }).then(function(response) {
+      //   console.log(response);
+
+      //   currentWeatherEl.addClass("today");
+
+      //   let cityNameEl = $("<h3>").addClass("city-name").text(`${cityName}, `);
+      //   let currentDate = moment();
+
+      //   let dateEl = $("<p>").text(currentDate.format("LL"));
+      //   let iconEl = $("<img>").attr(
+      //     "src",
+      //     `http://openweathermap.org/img/w/${response.weather[0].icon}.png`
+      //   );
+
+      //   currentWeatherEl.append(cityNameEl);
+      //   currentWeatherEl.append(dateEl);
+      //   currentWeatherEl.append(iconEl);
+
+      //   currentWeatherEl.append(
+      //     $("<div>").text("Tempreture: " + Math.round(response.main.temp) + "ºC")
+      //   );
+      //   currentWeatherEl.append(
+      //     $("<div>").text("Wind: " + response.wind.speed.toFixed(1) + " m/s")
+      //   );
+      //   currentWeatherEl.append(
+      //     $("<div>").text("Humidity: " + response.main.humidity + "%")
+      //   );
+      // });
+
+      // // 5-day forecast to display
+      // $.ajax({
+      //   url: buildForecastQueryURL(lat, lon),
+      //   method: "GET",
+      // }).then(function (response) {
+      //   response = response.list;
+      //   console.log(response);
+
+      //   let day1 = response.slice(0, 8);
+      //   console.log(day1);
+      //   let temp1 = [];
+      //   day1.forEach(function(item) {
+      //     temp1.push(item.main.temp)
+      //   })
+      //   console.log(temp1)
+      //   console.log(Math.round(Math.max(...temp1)));
+      //   console.log(Math.round(Math.min(...temp1)));
+
+      //   $("#card1 card-title").text(moment());
     });
   });
 });
